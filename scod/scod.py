9849fc37c4e846c51e0afe3ed04b0b57817093e9
Apoorva Sharma <apoorvaxsharma@gmail.com>
2021-12-14 20:00:17 -0800

    added code to compute and reguarlize kernels against GP kernels

--- scod/scod.py
+++ scod/scod.py
@@ -2,7 +2,7 @@ from typing import Union, Optional, Callable, Tuple, List
 from scod.sketching.sketched_pca import SRFT_SinglePassPCA
 import torch
 from torch import nn
-from copy import deepcopy
+from copy import deepcopy, copy
 from torch.cuda.amp.autocast_mode import autocast
 
 from tqdm.autonotebook import tqdm
@@ -117,6 +117,7 @@ class Projector(nn.Module):
 base_config = {
     'num_samples': None, # sketch size T (T)
     'num_eigs': 10, # low rank estimate to recover (k)
+    'per_parameter_prior': False, # if True, then have a separate scale for the prior variance per parameter of the base model
     'sketch_type': 'gaussian', # sketch type 
     'offline_proj_dim': None, # whether to subsample rows during offline computation
     'online_proj_dim': None, # whether to project output down before taking gradients at test time
@@ -158,11 +159,14 @@ class SCOD(nn.Module):
             self.trainable_params = list(filter(lambda x: x.requires_grad, self.model.parameters()))
         else:
             self.trainable_params = list(parameters)
-        self.n_params = int(sum(p.numel() for p in self.trainable_params))
-        print("Weight space dimension: %1.3e"%self.n_params)
+        self.n_weights_per_param = list(p.numel() for p in self.trainable_params)
+        self.n_weights = int(sum(self.n_weights_per_param))
+        print("Weight space dimension: %1.3e"%self.n_weights)
                 
         self.num_samples = self.config['num_samples']
         self.num_eigs = self.config['num_eigs']
+
+        self.per_parameter_prior = self.config['per_parameter_prior']
         
         if self.num_samples is None:
             self.num_samples = 6*self.num_eigs + 4
@@ -193,22 +197,44 @@ class SCOD(nn.Module):
         self.online_projection_type = self.config['online_proj_type']
  
 
-        self.projector = Projector(
-            N=self.n_params,
-            r=2*max(self.num_eigs + 2, (self.num_samples-1)//3),
-            device=self.device
-        )
+        # nn.Parameters:
+        #  low-rank approx of Gauss Newton, filled in by self.process_dataset
+        self.GGN_eigs = nn.Parameter(torch.zeros(self.num_eigs, device=self.device), requires_grad=False)
+        self.GGN_basis = nn.Parameter(torch.zeros(self.n_weights, self.num_eigs, device=self.device), requires_grad=False)
+        self.GGN_sqrt_prior_scales = nn.Parameter(torch.ones(len(self.trainable_params), device=self.device), requires_grad=False) # stores what prior scales were used in GGN approx
+        self.GGN_is_aligned = True
 
-        self.log_eps = nn.Parameter(torch.zeros(1, device=self.device), requires_grad=True) # prior variance
+        #  prior scale
+        n_priors = 1
+        if self.per_parameter_prior:
+            n_priors = len(self.trainable_params)
+        self.log_prior_scale = nn.Parameter(torch.zeros(n_priors, device=self.device), requires_grad=True)
+
         self.scaling_factor = nn.Parameter(torch.ones(1, device=self.device), requires_grad=False) # final scaling factor to rescale uncertainty output
-        self.hyperparameters = [self.log_eps]
+        self.hyperparameters = [self.log_prior_scale]
 
     @property
     def eps(self) -> torch.Tensor:
         """
         returns eps, the scale on the prior covariance
         """
-        return torch.exp(self.log_eps)
+        return torch.exp(self.log_prior_scale)
+
+    @property
+    def sqrt_prior_scales(self, as_vec=False) -> torch.Tensor:
+        """
+        returns sqrt prior scales as torch.Tensor of the same length as self.parameters 
+        """
+        sqrt_prior_scale = torch.exp(0.5*self.log_prior_scale).broadcast_to(len(self.trainable_params))
+        return sqrt_prior_scale
+
+    def prior_scales_to_vec(self, prior_scale):
+        """
+        converts a vector containing per parameter scales into a 
+        vector of dimension (n_weights) by repeating the scale for each param
+        the correct number of times
+        """
+        return torch.cat([s*torch.ones(n_p) for s, n_p in zip(prior_scale, self.n_weights_per_param)])
         
     def process_dataset(self, dataset : torch.utils.data.Dataset ) -> None:
         """
@@ -228,7 +254,7 @@ class SCOD(nn.Module):
                                                  num_workers=4,
                                                  pin_memory=True)
         
-        sketch = self.sketch_class(N=self.n_params, 
+        sketch = self.sketch_class(N=self.n_weights, 
                                    r=self.num_eigs,
                                    T=self.num_samples,
                                    device=self.device)
@@ -257,20 +283,24 @@ class SCOD(nn.Module):
                 if self.use_random_proj:
                     pre_jac_factor = random_subslice(pre_jac_factor, dim=0, k=self.offline_projection_dim, scale=True)
 
-            sqrt_C_T = self._get_weight_jacobian(pre_jac_factor) # shape ([T x N])
+            sqrt_C_T = self._get_weight_jacobian(pre_jac_factor, scaled_by_prior=True) # shape ([T x N])
 
             sketch.low_rank_update(sqrt_C_T.t()) # add C = sqrt_C sqrt_C^T to the sketch
         
         del sqrt_C_T # free memory @TODO: sketch could take output tensors to populate directly
         eigs, eigvs = sketch.eigs()
         del sketch
-        self.projector.process_basis(eigs, eigvs)
+        self.GGN_eigs.data = eigs
+        self.GGN_basis.data = eigvs
+        self.GGN_sqrt_prior_scales.data = copy(self.sqrt_prior_scales)
+        self.GGN_is_aligned = True
             
         self.configured.data = torch.ones(1, dtype=torch.bool)
 
     def optimize_output_projection(self, dataset : torch.utils.data.Dataset ) -> None:
         """
         Loops through dataset to determine best projection matrix to use for output compression
+        @TODO: this doesn't work, debug...
         """
         dataloader = torch.utils.data.DataLoader(dataset, 
                                                  batch_size=1, 
@@ -308,18 +338,71 @@ class SCOD(nn.Module):
         self.online_proj_basis = sketch.eigs()[1] # (output_shape x self.online_projection_dim) 
 
     
-    def _get_weight_jacobian(self, vec : torch.Tensor) -> torch.Tensor:
+    def _get_weight_jacobian(self, vec : torch.Tensor,
+                                   scaled_by_prior : bool = True) -> torch.Tensor:
         """
         returns k x nparam matrix, with each row being d(vec[i])/d(weights) for i = 1, ..., k
+
+        if scaled_by_prior is True, then the gradients are divided by the sqrt prior scale for that parameter
         """
         assert len(vec.shape) == 1
         grad_vecs = []
         for j in range(vec.shape[0]):
             grads = grad(vec[j], self.trainable_params, retain_graph=True, only_inputs=True, allow_unused=True)
-            g = torch.cat([gr.contiguous().view(-1) for gr in grads]).detach()
+            if scaled_by_prior:
+                grads = [gr.detach()*sqrt_ps for gr, sqrt_ps in zip(grads, self.sqrt_prior_scales)]
+            g = torch.cat([gr.contiguous().view(-1) for gr in grads])
             grad_vecs.append(g)
             
         return torch.stack(grad_vecs)
+
+    def _compute_output_diag_var(self, 
+                               J : torch.Tensor, 
+                               P : Optional[torch.Tensor] = None, 
+                               n_eigs : Optional[int] = None,
+                               prior_predictive : bool = False):
+        """
+        Assumes J is the jacobian already scaled by the sqrt prior covariance.
+
+        Returns diag( J ( I - U D U^T ) J^T )
+        where D = diag( eigs / (eigs + 1) )
+
+        and U diag(eigs) U^T ~= G, the Gauss Newton matrix computed using gradients
+        already scaled by the sqrt prior covariance
+
+        that is, we let J = (df/dw) \Sigma_0^{1/2} 
+        and G = sum( J_i^T H_i J_i )
+        """ 
+        JJT = J @ J.T # torch.sum(J**2, dim=-1)
+
+        if not self.configured or prior_predictive:
+            return torch.diagonal(JJT)
+
+        if n_eigs is None:
+            n_eigs = self.num_eigs
+
+        basis = self.GGN_basis[:,-n_eigs:]
+        eigs = torch.clamp( self.GGN_eigs[-n_eigs:], min=0.)
+
+        if self.GGN_is_aligned:
+            scaling = eigs / (eigs + 1.)  
+            neg_term = torch.sqrt(scaling[None,:])*(J @ basis)
+            neg_term = neg_term @ neg_term.T
+        else:
+            # assuming G = rescaling*U D (rescaling*U).T
+            # the rescaling breaks the orthogonality of the basis
+            rescaling = self.prior_scales_to_vec(self.sqrt_prior_scales/self.GGN_sqrt_prior_scales)
+            inv_term = torch.linalg.inv(torch.diag_embed(1./ eigs) + basis.T @ ((rescaling**2)[:, None] * basis))
+            scaled_jac = J @ (rescaling[:,None]*basis)
+            neg_term = scaled_jac @ inv_term @ scaled_jac.T
+        
+        if P is not None:
+            chol_sig = torch.linalg.cholesky(JJT - neg_term)
+            sig = torch.sum( (P @ chol_sig)**2, dim=-1)  #( P[:,None,:] @ ( (JJT - neg_term) @ P.T) )[:,0]
+            return sig
+        
+        else: 
+            return torch.diagonal((JJT - neg_term)) # shape (d,d)                       
 
     def output_projection(self, output_size, T):
         if self.online_projection_type == 'PCA':
@@ -333,7 +416,8 @@ class SCOD(nn.Module):
             P,_ = torch.linalg.qr(P, 'reduced')
             return P
             
-    def forward(self, inputs : torch.Tensor, 
+    def forward(self, inputs : torch.Tensor,
+                      use_prior : bool = False, 
                       n_eigs : Optional[int] = None, 
                       T : Optional[int] = None) -> Tuple[ List[torch.distributions.Distribution], torch.Tensor ] :
         """
@@ -345,9 +429,9 @@ class SCOD(nn.Module):
             mu = model(inputs) list of N distribution objects
             unc = hessian based uncertainty estimates shape (N), torch.Tensor
         """
-        if not self.configured:
-            print("Must call process_dataset first before using model for predictions.")
-            raise NotImplementedError
+        # if not self.configured:
+        #     print("Must call process_dataset first before using model for predictions.")
+        #     raise NotImplementedError
 
         if T is None:
             T = self.online_projection_dim
@@ -375,18 +459,15 @@ class SCOD(nn.Module):
 
         if T is not None and T < flat_z.shape[-1]:
             # if projection dim is provided and less than original dim
-            # @TODO: generate P through a function call depending on
-            # self.online_proj_type
             P = self.output_projection(flat_z_shape, T)
             proj_flat_z = flat_z @ P
+
         unc = []
         dists = []
-
         for j in range(N):
-            J_proj_flat_z = self._get_weight_jacobian(proj_flat_z[j,:]).detach()
-            z_flat_var = self.projector.compute_diag_var(J_proj_flat_z, P=P, n_eigs=n_eigs, eps=self.eps)
+            J_proj_flat_z = self._get_weight_jacobian(proj_flat_z[j,:], scaled_by_prior=True)
+            z_flat_var = self._compute_output_diag_var(J_proj_flat_z, P=P, n_eigs=n_eigs, prior_predictive=use_prior)
             z_var = z_flat_var.view(z[j,...].shape)
-
             dist = self.dist_constructor(z[j,...])
             output_dist = dist.marginalize(z_var)
 
@@ -396,7 +477,44 @@ class SCOD(nn.Module):
         unc = torch.stack(unc)
         return dists, unc
 
-    def optimize_nll(self, 
+    def kernel_matrix(self, inputs, use_prior=False, n_eigs=None):
+        """
+        returns a kernel (gram) matrix for the set of inputs
+        
+        returns a matrix of size: [KD x KD] where inputs is [X, d]
+        """
+        N = inputs.shape[0] # batch size
+
+        z = self.model(inputs) # batch of outputs
+        flat_z = z.view(N, -1) # batch of flattened outputs
+        jacs = []
+        for j in range(N):
+            J = self._get_weight_jacobian(flat_z[j,:], scaled_by_prior=True)
+            jacs.append(J)
+
+        jacs = torch.cat(jacs, dim=0) #(KD x N)
+        
+        JJT = jacs @ jacs.T
+        if use_prior:
+            return JJT
+
+        rescaling = self.prior_scales_to_vec(self.sqrt_prior_scales/self.GGN_sqrt_prior_scales)
+        rescaling_sq_norm = torch.sum(rescaling**2)
+
+        if n_eigs is None:
+            n_eigs = self.num_eigs
+
+        # extract basis and eigs scaled to match current prior scale
+        basis = rescaling[:,None]*self.GGN_basis[:,-n_eigs:] / torch.sqrt(rescaling_sq_norm)
+        eigs = rescaling_sq_norm*torch.clamp( self.GGN_eigs[-n_eigs:], min=0.)
+        
+        scaling = eigs / (eigs + 1.)  
+        neg_term = torch.sqrt(scaling[None,:])*(jacs @ basis)
+        neg_term = neg_term @ neg_term.T
+        
+        return JJT - neg_term
+
+    def optimize_prior_scale_by_nll(self, 
                      dataset : torch.utils.data.Dataset,
                      num_epochs : int = 2,
                      batch_size : int = 20):
@@ -404,11 +522,14 @@ class SCOD(nn.Module):
         tunes prior variance scale (eps) via SGD to minimize 
         validation nll on a given dataset
         """
+        self.GGN_is_aligned = False
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
                                              shuffle=True, num_workers=4, pin_memory=True)
         
         dataset_size = len(dataset)
-        optimizer = torch.optim.Adam(self.hyperparameters, lr=1.)
+        optimizer = torch.optim.Adam(self.hyperparameters, lr=1e-1)
+
+        losses = []
 
         with tqdm(total=num_epochs, position=0) as pbar:
             pbar2 = tqdm(total=dataset_size, position=1)
@@ -426,7 +547,7 @@ class SCOD(nn.Module):
                     dists, _ = self.forward(inputs)
                     loss = 0
                     for dist, label in zip(dists, labels):
-                        loss += -dist.validated_log_prob(label).mean()
+                        loss += -dist.log_prob(label).mean() # -dist.validated_log_prob(label).mean()
 
                     
                     loss /= len(dists)
@@ -434,11 +555,84 @@ class SCOD(nn.Module):
                     loss.backward()
                     optimizer.step()
                     
-                    pbar2.set_postfix(batch_loss=loss.item(), eps=self.eps.item())
+                    pbar2.set_postfix(batch_loss=loss.item(), eps=self.eps.mean().item())
                     pbar2.update(inputs.shape[0])
+
+                    losses.append(loss.item())
             
-                pbar.set_postfix(eps=self.eps.item())
+                pbar.set_postfix(eps=self.eps.mean().item())
                 pbar.update(1)
+
+        return losses
+
+    def optimize_prior_scale_by_GP_kernel(self, 
+                     dataset : torch.utils.data.Dataset,
+                     GP_kernel : Callable[[torch.Tensor],torch.Tensor],
+                     GP_mu : Union[Callable[[torch.Tensor],torch.Tensor], float] = 0,
+                     num_epochs : int = 20,
+                     batch_size : int = 5,
+                     grad_accumulation_steps : int = 5):
+        """
+        tunes prior variance scale (eps) via SGD to minimize 
+        difference between prior and a given GP
+        GP_kernel should take in a batch of data and produce a gram matrix
+        """
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                             shuffle=True, num_workers=4, pin_memory=True)
+        
+        dataset_size = len(dataset)
+        optimizer = torch.optim.Adam(self.hyperparameters, lr=1e-1)
+
+        losses = []
+
+        grad_counter = 0
+        with tqdm(total=num_epochs, position=0) as pbar:
+            pbar2 = tqdm(total=dataset_size, position=1)
+            for epoch in range(num_epochs):
+                pbar2.refresh()
+                pbar2.reset(total=dataset_size)
+                for inputs, labels in dataloader:                
+                    inputs = inputs.to(self.device, non_blocking=True)
+                    labels = labels.to(self.device, non_blocking=True)
+
+                    mu_dnn = self.model(inputs)
+                    K_dnn = self.kernel_matrix(inputs, use_prior=True)
+                    K_dnn_inv = torch.linalg.inv(K_dnn + 1e-5*torch.eye(K_dnn.shape[0]))
+                    K_gp = GP_kernel(inputs).to(self.device, non_blocking=True)
+                    K_ratio = K_dnn_inv @ K_gp
+                    if callable(GP_mu):
+                        mu_gp = GP_mu(inputs)
+                    else:
+                        mu_gp = GP_mu * torch.ones_like(mu_dnn)
+
+                    err = (mu_dnn - mu_gp).view(-1)
+
+                    loss = torch.trace(K_ratio) # tr K_dnn^{-1} K_gp
+                    # loss += torch.dot(err, K_dnn_inv @ err) # (mu_dnn - mu_gp)^T K_dnn^{-1} (mu_dnn - mu_gp)
+                    loss -= err.shape[-1] # dimension of multivariate Guassian
+                    loss -= torch.slogdet(K_ratio).logabsdet # log det ( K_dnn^{-1} K_gp )
+
+                    loss = loss / grad_accumulation_steps
+                    
+                    loss.backward()
+
+                    grad_counter += 1
+
+                    if grad_counter % grad_accumulation_steps == 0:
+                        # after grad_accumulation steps have passed, then take the gradient step
+                        optimizer.step()
+                        # zero the parameter gradients
+                        optimizer.zero_grad()
+                    
+                    pbar2.set_postfix(batch_loss=loss.item(), eps=self.eps.mean().item())
+                    pbar2.update(inputs.shape[0])
+
+                    losses.append(loss.item())
+            
+                pbar.set_postfix(eps=self.eps.mean().item())
+                pbar.update(1)
+
+        return losses
 
     def optimize_entropy_separation(self,
                                     val_dataset : torch.utils.data.Dataset,
